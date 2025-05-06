8b44fdf5162cc08340cd96e4d93e6ba894466b06
Santi515 <santiago.altagaveite@gmail.com>
2025-05-06 14:48:20 +0200

    Borre lo solicitado y se agregaron cosas nuevas a lo largo del dia

--- irg-frontend/app/_layout.tsx
+++ irg-frontend/app/_layout.tsx
@@ -1,29 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Slot, SplashScreen } from 'expo-router';
+import { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded] = useFonts({
+    Playfair: require('../assets/fonts/PlayfairDisplay-Regular.ttf'),
+    Inter: require('../assets/fonts/Inter-Regular.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  useEffect(() => {
+    if (!fontsLoaded) SplashScreen.preventAutoHideAsync();
+    else SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <>
+      <StatusBar
+        backgroundColor={"#212121"}
+        barStyle="light-content"
+      />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#212121' }}>
+        <Slot />
+      </SafeAreaView>
+    </>
   );
 }
